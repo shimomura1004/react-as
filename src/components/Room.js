113e85bb85e2a2d8844162b1874ff21f824e6602
SHIMOMURA Sho <graueule@gmail.com>
2019-02-27 00:39:44 -0800

    add simple appbar

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import io from 'socket.io-client'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import SendIcon from '@material-ui/icons/Send';
 import Message from './Message';
 import '../styles/room.css';
@@ -29,10 +33,19 @@ export default class Room extends React.Component {
         let messages = this.props.messages;
         return (
             <div>
+                <AppBar className="root" position="fixed" color="primary">
+                    <Toolbar>
+                        <IconButton className="menuButton" color="inherit" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography className="grow" variant="h6" color="inherit">
+                            {this.props.room && this.props.room.name}
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
                 <div className="chat-body" onClick={() =>
                     this.props.loadMessages(this.props.loading, this.props.api_key, this.props.room.id, messages[0][0].id)
                 }>
-                    <p>{this.props.room && this.props.room.name}</p>
                     <p>{this.props.loading ? 'LOADING' : 'LOADED'}</p>
                     <div>
                         {messages.map((message) => <Message key={message[0].id} message={message} />)}
@@ -55,11 +68,11 @@ export default class Room extends React.Component {
                         }}
                     />
                     <div className="send-button">
-                    <IconButton aria-label="Send" onClick={(e) => {
-                        this.props.postMessage(this.props.api_key, this.props.room.id, this.props.textfield);
-                    }}>
-                        <SendIcon />
-                    </IconButton>
+                        <IconButton aria-label="Send" onClick={(e) => {
+                            this.props.postMessage(this.props.api_key, this.props.room.id, this.props.textfield);
+                        }}>
+                            <SendIcon />
+                        </IconButton>
                     </div>
                 </div>
             </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import blue from '@material-ui/core/colors/blue';
 import Room from '../containers/Room.js';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: red,
+    secondary: blue,
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Room />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Room />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
