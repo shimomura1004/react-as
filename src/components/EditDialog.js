04a8df5bc67ab7593e261f86362a9c1ca6b8fba2
SHIMOMURA Sho <graueule@gmail.com>
2019-03-24 22:26:00 -0700

    remove overflow-x hidden to avoid confliction with full-screen dialog

--- src/components/EditDialog.js
+++ src/components/EditDialog.js
@@ -1,22 +1,57 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import Slide from '@material-ui/core/Slide';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
 
 function Transition(props) {
     return <Slide direction="up" {...props} />;
 }
 
-export default class Message extends React.Component {
+export default class EditDialog extends React.Component {
     render() {
-        return (
+        let message = this.props.message;
+
+        return (<div>
             <Dialog
                 fullScreen
                 open={this.props.open}
                 TransitionComponent={Transition}
             >
-                <h1 onClick={this.props.handleClose}>DIALOG!</h1>
-                <p>{this.props.message && this.props.message.body}</p>
+                <AppBar>
+                    <Toolbar>
+                        <IconButton color="inherit" onClick={this.props.handleClose} aria-label="Close">
+                            <CloseIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit">
+                            Message
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                {
+                    message
+                        ?   <div className="message" style={{marginTop: "3.5em"}}>
+                                <img className="icon" src={message.profile_image_url} alt={message.name} />
+                                <div className="content">
+                                    <div className="profile">
+                                        <span className="user-name">{message.name}</span>
+                                    </div>
+                                    <p
+                                        className="body"
+                                        dangerouslySetInnerHTML={{__html: message.html_body}}
+                                    >
+                                    </p>
+                                    <br/>
+                                    <p>{message.body}</p>
+                                </div>
+                            </div>
+                        :   <div />
+                }
             </Dialog>
+            </div>
         );
     }
 };
