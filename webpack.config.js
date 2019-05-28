a231d5c31f0981d6f154c83ef8edac071e732266
SHIMOMURA Sho <graueule@gmail.com>
2019-05-28 20:10:51 +0900

    introduce typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require('copy-webpack-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   module: {
@@ -10,6 +11,13 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "awesome-typescript-loader"
         }
       },
       {
@@ -30,5 +38,10 @@ module.exports = {
       swSrc: './src/service-worker.js',
       swDest: 'service-worker.js'
     })
-  ]
+  ],
+  node: {
+    fs: 'empty',
+    net: 'empty'
+  },
+  devtool: 'source-map'
 };
