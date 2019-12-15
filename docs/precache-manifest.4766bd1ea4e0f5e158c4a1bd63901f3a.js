aad33c9b61077b62f7d8383ff9606c55239a0f9b
SHIMOMURA Sho <graueule@gmail.com>
2019-12-16 00:51:08 +0900

    deploy on github pages

---
+++ docs/precache-manifest.4766bd1ea4e0f5e158c4a1bd63901f3a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fe887cc5670f20427e16a0d97de36f",
+    "url": "as_icon.png"
+  },
+  {
+    "revision": "c92b85a5b907c70211f4ec25e29a8c4a",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "e496055dfd5d740a99fdfc292cefdf2b",
+    "url": "index.html"
+  },
+  {
+    "revision": "62a0bb8287de8ff81bd36bb02bbcfd43",
+    "url": "ipad_splash.png"
+  },
+  {
+    "revision": "c29636a17a8bdbfac98e73f8d6100f22",
+    "url": "ipadpro1_splash.png"
+  },
+  {
+    "revision": "f0c3797c47b8ac89376769abdd1a6922",
+    "url": "ipadpro2_splash.png"
+  },
+  {
+    "revision": "f71bc5c3bc90ee53a69489f795ffb501",
+    "url": "ipadpro3_splash.png"
+  },
+  {
+    "revision": "2c8dc11be756b21a874d650559097101",
+    "url": "iphone5_splash.png"
+  },
+  {
+    "revision": "e49d5c320e43e9b8cf076d0a4df2e428",
+    "url": "iphone6_splash.png"
+  },
+  {
+    "revision": "efc01f8adea712c2e39148eeced348d7",
+    "url": "iphoneplus_splash.png"
+  },
+  {
+    "revision": "9953d3e0f0af8746b1ebef2e7d43ee0e",
+    "url": "iphonex_splash.png"
+  },
+  {
+    "revision": "abae88628b0d62d1215f891c0bb19e66",
+    "url": "iphonexr_splash.png"
+  },
+  {
+    "revision": "b98b207905c9b830ed5e071caa2cb140",
+    "url": "iphonexsmax_splash.png"
+  },
+  {
+    "revision": "4dfd9e50552ede4fec8a",
+    "url": "main.js"
+  },
+  {
+    "revision": "dcb7116645bfca2e0a8da9d048d29ca6",
+    "url": "manifest.json"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,33 @@
+importScripts("precache-manifest.4766bd1ea4e0f5e158c4a1bd63901f3a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js');
+
+if (workbox) {
+    workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+    workbox.routing.registerRoute(
+        /\.(?:png|jpg|jpeg|svg|gif)$/,
+        new workbox.strategies.NetworkFirst({
+            cacheName: 'image-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 20,
+                    maxAgeSeconds: 7 * 24 * 60 * 60,
+                })
+            ],
+        })
+    );
+
+    workbox.routing.registerRoute(
+        new RegExp('/api/v1/room/list.json'),
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        new RegExp('/api/v1/message/list.json'),
+        new workbox.strategies.NetworkFirst()
+    );
+}
+else {
+    console.log("Workbox didn't load");
+}
+
